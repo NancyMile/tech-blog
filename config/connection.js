1dfd23edf6fc239276844d9d89e76a1b6b200de5
nancy <smithnancy@outlook.com>
2022-09-10 18:52:28 +1000

    WIP

--- config/connection.js
+++ config/connection.js
@@ -6,16 +6,32 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  const { URL } = require('url');
+  // const url = "mysql://b5158a0b83c7d7:36f89d54@us-cdbr-east-06.cleardb.net/heroku_cdd542e963f6ec2?reconnect=true";
+  const url = process.env.CLEARDB_DATABASE_URL;
+  const parsedUrl = new URL(url);
+
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    parsedUrl.pathname,
+    parsedUrl.username,
+    parsedUrl.password,
     {
-      host: 'us-cdbr-east-06.cleardb.net',
-      dialect: 'mysql',
-      port: 3306
+      host: parsedUrl.hostname,
+      dialect: parsedUrl.protocol,
+      port: parsedUrl.port
     }
   );
+  
+  // sequelize = new Sequelize(
+  //   process.env.DB_NAME,
+  //   process.env.DB_USER,
+  //   process.env.DB_PASSWORD,
+  //   {
+  //     host: 'us-cdbr-east-06.cleardb.net',
+  //     dialect: 'mysql',
+  //     port: 3306
+  //   }
+  // );
 }
 
 module.exports = sequelize;
