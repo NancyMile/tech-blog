273cec641a4b6139f8e2b3e3c080c93ac403835b
nancy <smithnancy@outlook.com>
2022-09-10 13:59:55 +1000

    route delete condition

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -20,8 +20,8 @@ router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
       where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
+        id: req.params.id
+        //user_id: req.session.user_id,
       },
     });
     if (!postData) {
