d71f95c771296d423b93c806a90af7d9e2e05c1a
nancy <smithnancy@outlook.com>
2022-09-10 18:59:19 +1000

    Connection parsing variables for db connection.

--- config/connection.js
+++ config/connection.js
@@ -5,33 +5,35 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  const { URL } = require('url');
+} else if (process.env.CLEARDB_DATABASE_URL) {
   // const url = "mysql://b5158a0b83c7d7:36f89d54@us-cdbr-east-06.cleardb.net/heroku_cdd542e963f6ec2?reconnect=true";
+  const { URL } = require('url');
   const url = process.env.CLEARDB_DATABASE_URL;
   const parsedUrl = new URL(url);
 
+console.log(parsedUrl)
+
   sequelize = new Sequelize(
-    parsedUrl.pathname,
+    parsedUrl.pathname.replace('/', ''),
     parsedUrl.username,
     parsedUrl.password,
     {
       host: parsedUrl.hostname,
-      dialect: parsedUrl.protocol,
-      port: parsedUrl.port
+      dialect: 'mysql', //parsedUrl.protocol,
+      port: 3306
     }
   );
-  
-  // sequelize = new Sequelize(
-  //   process.env.DB_NAME,
-  //   process.env.DB_USER,
-  //   process.env.DB_PASSWORD,
-  //   {
-  //     host: 'us-cdbr-east-06.cleardb.net',
-  //     dialect: 'mysql',
-  //     port: 3306
-  //   }
-  // );
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
 }
 
 module.exports = sequelize;
